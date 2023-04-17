db174819e5d6230bcb2e188ab1fefad37acd3dd2
Saurabh Dayal <saurabhdayal11@gmail.com>
2023-04-17 15:03:37 +0530

    once again

--- src/services_subadmin.rs
+++ src/services_subadmin.rs
@@ -47,19 +47,9 @@ async fn create_dish(state: Data<AppState>, dish: web::Json<Dishes>, usr:UserAut
     let d = dish.into_inner();
     let role_row = usr.roles;
 
-    let mut r = "".to_string();
-    for role in role_row{
-
-        if role.role_type =="SubAdmin".to_string(){
-            r = "Admin".to_string();
-        }
-        if role.role_type =="Admin".to_string(){
-            r = "Admin".to_string();
-            break;
-        }
-    }
-
-    if r =="Admin".to_string()||r=="SubAdmin".to_string() {
+    if role_row.iter().any(|x| {
+        x.role_type == "Admin"|| x.role_type == "SubAdmin"
+    }) {
         let row = sqlx::query_as!( Dishes,
             "INSERT INTO dishes (dish_name, dish_cost, restaurant_id, user_id) VALUES ($1, $2, $3, $4) 
             RETURNING dish_id, dish_name, dish_cost, restaurant_id, user_id",
@@ -82,19 +72,9 @@ async fn get_restaurant_by_user_id(state: Data<AppState>, usr: UserAuth) -> Resu
 
     let role_row = usr.roles;
 
-    let mut r = "".to_string();
-
-    for role in role_row{
-        if role.role_type =="SubAdmin".to_string(){
-            r = "SubAdmin".to_string();
-        }
-        if role.role_type =="Admin".to_string(){
-            r = "Admin".to_string();
-            break;
-        }
-    }
-
-    if r=="Admin".to_string(){
+    if role_row.iter().any(|x| {
+        x.role_type == "Admin"
+    }){
         let rows = sqlx::query_as!(Restaurants,
             "SELECT restaurant_id, restaurant_name, restaurant_address, user_id 
             FROM Restaurants"
@@ -102,7 +82,9 @@ async fn get_restaurant_by_user_id(state: Data<AppState>, usr: UserAuth) -> Resu
         .fetch_all(&state.db)
         .await?;
         Ok(HttpResponse::Ok().json(rows))
-    } else if r=="SubAdmin".to_string() {
+    } else if role_row.iter().any(|x| {
+        x.role_type == "SubAdmin"
+    }){
         let rows = sqlx::query_as!(Restaurants,
             "SELECT restaurant_id, restaurant_name, restaurant_address, user_id 
             FROM Restaurants WHERE user_id=$1", usr.user_id
@@ -123,20 +105,10 @@ async fn get_restaurant_by_user_id(state: Data<AppState>, usr: UserAuth) -> Resu
 async fn get_dish_by_user_id(state: Data<AppState>, usr: UserAuth) -> Result<impl Responder, MyError> {
     
     let role_row = usr.roles;
-    
-    let mut r = "".to_string();
-    for role in role_row{
 
-        if role.role_type =="SubAdmin".to_string(){
-            r = "Admin".to_string();
-        }
-        if role.role_type =="Admin".to_string(){
-            r = "Admin".to_string();
-            break;
-        }
-    }
-
-    if r=="Admin".to_string(){
+    if role_row.iter().any(|x| {
+        x.role_type == "Admin"
+    }){
         let rows = sqlx::query_as!(Dishes,
             "SELECT dish_id, dish_name, dish_cost, restaurant_id, user_id 
             FROM dishes"
@@ -144,7 +116,9 @@ async fn get_dish_by_user_id(state: Data<AppState>, usr: UserAuth) -> Result<imp
         .fetch_all(&state.db)
         .await?;
         Ok(HttpResponse::Ok().json(rows))
-    } else if r=="SubAdmin".to_string() {
+    } else if role_row.iter().any(|x| {
+        x.role_type == "SubAdmin"
+    }) {
         let rows = sqlx::query_as!(Dishes,
             "SELECT dish_id, dish_name, dish_cost, restaurant_id, user_id 
             FROM dishes WHERE user_id=$1", usr.user_id
@@ -160,25 +134,16 @@ async fn get_dish_by_user_id(state: Data<AppState>, usr: UserAuth) -> Result<imp
 
 
 
+
 // Get all users 
 #[get("/users")]
 async fn get_users_list(state: Data<AppState>, usr:UserAuth) -> Result<impl Responder, MyError> {
 
     let role_row = usr.roles;
 
-    let mut r = "".to_string();
-    for role in role_row{
-
-        if role.role_type =="SubAdmin".to_string(){
-            r = "Admin".to_string();
-        }
-        if role.role_type =="Admin".to_string(){
-            r = "Admin".to_string();
-            break;
-        }
-    }
-
-    if r =="Admin".to_string()||r =="SubAdmin".to_string() {
+    if role_row.iter().any(|x| {
+        x.role_type == "Admin"|| x.role_type == "SubAdmin"
+    }) {
         let row = sqlx::query_as!( Users,
             "SELECT user_id, user_name, user_password, user_email FROM users"
         )
